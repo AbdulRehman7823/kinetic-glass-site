387a05f96e72fc22a417921101dbbdb3e9f52307
abdulman7823 <abdulman4401@gmail.com>
2023-01-11 05:58:48 +0500

    edit-git-url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/kineticGlass/kinetic-glass-site',
+            'https://github.com/kineticGlass/kinetic-glass-site/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/kineticGlass/kinetic-glass-site',
+            'https://github.com/kineticGlass/kinetic-glass-site/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
